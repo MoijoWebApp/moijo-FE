453d29397dac4a672196473dab44f2d8e3cc4291
Dobbymin <gmin990624@gmail.com>
2025-05-01 14:38:23 +0900

    feat:  storybook 세팅 구현

---
+++ vitest.workspace.ts
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-nodejs-modules */
+import { storybookTest } from '@storybook/experimental-addon-test/vitest-plugin';
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+import { defineWorkspace } from 'vitest/config';
+
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/writing-tests/test-addon
+export default defineWorkspace([
+  'vite.config.ts',
+  {
+    extends: 'vite.config.ts',
+    plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+      storybookTest({ configDir: path.join(dirname, '.storybook') }),
+    ],
+    test: {
+      name: 'storybook',
+      browser: {
+        enabled: true,
+        headless: true,
+        name: 'chromium',
+        provider: 'playwright',
+      },
+      setupFiles: ['.storybook/vitest.setup.ts'],
+    },
+  },
+]);
